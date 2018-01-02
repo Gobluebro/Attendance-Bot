fe30cb4cbbc1edc51b6c30cfc0ed87cc98aa57d5
Griffin Strayer <griffinstrayer+coding@gmail.com>
2018-01-02 12:31:04 -0500

    Added saving after using attendstop command. attendadd adds a person to the list, still need to do the date part. attendremove removes a person from the specified date. attenddeletemonth deletes a month file which holds all of the names and dates recorded of that month.

--- index.js
+++ index.js
@@ -3,11 +3,48 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require('fs');
 
-//check the date
-//open that month's file or create a new one
+function saveAttendance(attendanceArr){
+    //make sure there is something in the array
+    //and make sure it's not null or undefined
+    if (attendanceArr[0] != null){
+        var today = new Date();
+        var month = today.getMonth()+1; //january is 0
+        var day = today.getDate(); //getday returns the day of the week.
+        var year = today.getFullYear(); //gets the 4 digit version
+        thisMonth = year + '-' + month;
+        thisDay = year + '/' +  month + '/' + day;
+        attendanceArr.unshift(thisDay);
+
+        //w -open file for writing. the file is created (if it does not exist) or truncated (if it exists).
+        //wx - like w but fails if path exists.
+        fs.open('./logs/' + thisMonth + '.txt', 'wx', (err, fd) =>{
+            if (err){
+                //if the file exists already append the array to it
+                if(err.code === 'EEXIST'){
+                    //flags a means append
+                    var appendExistingStream = fs.createWriteStream('./logs/' + thisMonth + '.txt', {flags:'a'});
+                    for (var i = 0; i < attendanceArr.length; i++){
+                        appendExistingStream.write(attendanceArr[i] + '\n');
+                    }
+                    appendExistingStream.end();
+                    return;
+                }
+                throw err;
+                return;
+            }
+            //defaults to just writing not appending.
+            var newFileStream = fs.createWriteStream('./logs/' + thisMonth + '.txt');
+            for (var i = 0; i < attendanceArr.length; i++){
+                newFileStream.write(attendanceArr[i] + '\n');
+            }
+            newFileStream.end();
+            return;
+        });
+    } 
+}
 
 client.on('ready', () => {
-    
+
 });
 
 client.on("message", async message => {
@@ -45,34 +82,83 @@ client.on("message", async message => {
             //#endregion
             //#region attenddeletemonth
             case "attenddeletemonth":
-                try{
-                    message.channel.send(`The attendance for ${args[0]} has been removed.`);
-                } catch (e) {
-                    message.channel.send(`An Error occurred. Make sure format is in year/month e.g. "!attenddeletemonth 2017/12".`);
-                    console.error('There was an error deleting a month!', e);
-                }
+                var thisMonth = args[0].split('/')[0] + '-' + args[0].split('/')[1];
+                fs.unlink('./logs/' + thisMonth + '.txt', function(err){
+                    if(err){
+                        if(err.code === 'ENOENT'){
+                            return message.reply(`File not found
+Make sure format is in year/month e.g. "!attenddeletemonth 2017/12".`);
+                        }
+                    }
+                    return message.reply(`The attendance for ${args[0]} has been removed.`);  
+                });
                 break;
             //#endregion
             //#region attendadd
             case "attendadd":
-                try{
-                    //add to the file
-                    message.channel.send(`${args[0]} has been added to the record on ${args[1]}.`);
-                } catch (e) {
-                    message.channel.send(`An error has occured while trying to add a user to the attendance file.`);
-                    console.error('There was an error trying to add to the file!', e);
+                //append to the file
+                if (config.isRecording){
+                    message.reply('Please wait until recording is finished to add someone.');
+                } else {
+                    var thisMonth = args[0].split('/')[0] + '-' + args[0].split('/')[1];
+                    fs.open('./logs/' + thisMonth + '.txt', 'wx', (err, fd) =>{
+                        if (err){
+                            //if the file exists already append the array to it
+                            if(err.code === 'EEXIST'){
+                                //flags a means append
+                                var addUserStream = fs.createWriteStream('./logs/' + thisMonth + '.txt', {flags:'a'});
+                                addUserStream.write(args[0] + '\n');
+                                addUserStream.write(args[1] + '\n');
+                                addUserStream.end();
+                                return;
+                            } else {
+                                return message.reply(`That file does not exist. 
+    Make sure you have the correct format e.g, !attendadd 2017/12/25 username`)
+                            }
+                            throw err;
+                            return;
+                        }
+                        return message.reply(`${args[1]} has been added to the record on ${args[0]}.`);
+                    });
                 }
                 break;
             //#endregion
             //#region attendremove
             case "attendremove":
-                try{
                     //remove from the file
-                    message.channel.send(`${args[0]} has been removed from the recording on ${args[1]}`);
-                } catch (e) {
-                    message.channel.send(`An error has occured while trying to remove a user to the attendance file.`);
-                    console.error('There was an error trying to remove from the file!', e);
-                }
+                    var thisMonth = args[0].split('/')[0] + '-' + args[0].split('/')[1];
+                    fs.open('./logs/' + thisMonth + '.txt', 'r', (err, fd) =>{
+                        if(err){
+                            if(err.code === 'ENOENT'){
+                                return message.reply(`That file does not exist. 
+Make sure you have the correct format e.g, !attendadd 2017/12/25 username`);
+                            }
+                            throw err;
+                        }
+                        fs.readFile('./logs/' + thisMonth + '.txt', 'utf8', function(err, data){
+                            if (err) throw err;
+                            var wholeFile = data.toString();
+                            var firstPartFile = wholeFile.substring(0, wholeFile.indexOf(args[0]));
+                            var lastPartFile = wholeFile.substring(wholeFile.indexOf(args[0]), wholeFile.length);
+                            var currentDayIndex = lastPartFile.indexOf(args[0]);
+                            var nextDay = parseInt(args[0].split('/')[2], 10) + 1;
+                            var nextDayFull = args[0].split('/')[0] + '/' + args[0].split('/')[1] + '/' + nextDay.toString();
+                            var nextDayIndex = lastPartFile.indexOf(nextDayFull);
+                            //making sure I don't accidentally delete a user from a differnt date
+                            if (nextDayIndex < lastPartFile.indexOf(args[1])){
+                                return message.reply('The user you specified is not found on that date.');
+                            } else {
+                                //remove the user, remove the empty line, combine them together with the stuff before.
+                                var removeUser = lastPartFile.replace(args[1], '');
+                                var combine = firstPartFile + removeUser.replace(/(\r\n|\n|\r)/, '');
+                                console.log(combine);
+                                fs.writeFile('./logs/' + thisMonth + '.txt', combine, 'utf8', (err) => {
+                                     if (err) throw err;
+                                     return message.reply(`${args[1]} has been removed from the recording on ${args[0]}`);
+                                });
+                            }
+                        });
+                    });  
                 break;
             //#endregion
             //#region attendhelp
@@ -102,10 +188,11 @@ client.on("message", async message => {
                     if (config.isRecording){
                         return message.reply('Attendance Bot is already recording.');
                     } else {
+                        config.attendanceArray = [];
                         config.isRecording = true;
                         //start the recording
                         return message.channel.send(`Attendance recording has started. 
-    Please enter the command "!entry" to have your name recorded for attendance.`);
+Please enter the command "!entry" to have your name recorded for attendance.`);
                     }
                 }   
                 break;
@@ -118,7 +205,7 @@ client.on("message", async message => {
                     if (config.isRecording){
                         //stop the recording and save 
                         config.isRecording = false;
-                        fs.writeFile("./config.json", JSON.stringify(config, null, 2), (err) => console.error);
+                        saveAttendance(config.attendanceArray);
                         return message.channel.send(`Attendance has stopped recording.`);
                     } else {
                         return message.reply(`Attendance Bot is not currently recording. If you wish to start the recording please use the command "!attendstart" to start.`);
@@ -129,7 +216,6 @@ client.on("message", async message => {
             //#endregion
     
             //#region automation
-    
             //#region attendtimestart
             case "attendtimestart":
                 if (config.isAutomated){
@@ -163,24 +249,23 @@ client.on("message", async message => {
                     return message.reply("Please provide a number between 1 and 60 for the length of attendance in minutes.");
                 break;
             //#endregion
-            
             //#endregion
         }
     }
     //#endregion
 
     //#region All users commands
-    if (command === entry){
+    if (command === "entry"){
         if (config.isRecording){
             //only enter their username if they haven't entered today already
             if (config.attendanceArray.includes(message.author.username)){
+                return message.reply("You have already entered today.");
+            } else {
                 //getting their username
                 config.attendanceArray.push(message.author.username);
-            } else {
-                return message.reply("You have already entered today.");
             }
         } else {
-            message.channel.send(`Attendance bot is not currently recording.
+            return message.reply(`Attendance bot is not currently recording.
     Please contact a GM for help`);
         }
     }
