53faba1408ac1b9543201be034ad8cda9faf4738
Griffin Strayer <griffinstrayer@gmail.com>
2018-09-10 22:06:21 -0400

    Is the date included error handling

--- commands/userRemove.js
+++ commands/userRemove.js
@@ -30,59 +30,63 @@ exports.run = (client, message, args) => {
     fs.readFile('./logs/' + thisMonth + '.txt', 'utf8', function(err, data) {
       if (err) throw err;
       var wholeFile = data.toString();
-      var firstPartFile = wholeFile.substring(0, wholeFile.indexOf(thisDate));
-      var lastPartFile = wholeFile.substring(
-        wholeFile.indexOf(thisDate),
-        wholeFile.length
-      );
-      var currentDayIndex = lastPartFile.indexOf(thisDate);
-      var nextDay = parseInt(theDay, 10) + 1;
-      var nextDayFull = theYear + '/' + theMonth + '/' + nextDay.toString();
-      var nextDayIndex = lastPartFile.indexOf(nextDayFull);
-      var wholeDay = '';
-      var removeUser = '';
-      var combine = '';
-      //making sure I don't accidentally delete a user from a differnt date
-      if (nextDayIndex === -1) {
-        wholeDay = lastPartFile;
-        //if user is not found on this date.
-        if (wholeDay.indexOf(args[1]) === -1) {
-          return message.reply(
-            'The user you specified is not found on that date.'
-          );
-        }
-        //remove the user, remove the empty line, combine them together with the stuff before.
-        removeUserArr = wholeDay.split('\r\n');
-        removeUserArr.splice(removeUserArr.indexOf(args[1]), 1);
-        removeUser = removeUserArr.join('\r\n');
-        combine = firstPartFile + removeUser;
+      if (wholeFile.includes(thisDate)) {
+        return message.reply('This date has no entries.');
       } else {
-        wholeDay = lastPartFile.substring(currentDayIndex, nextDayIndex - 1);
-        //if user is not found on this date.
-        if (wholeDay.indexOf(args[1]) === -1) {
-          return message.reply(
-            'The user you specified is not found on that date.'
-          );
+        var firstPartFile = wholeFile.substring(0, wholeFile.indexOf(thisDate));
+        var lastPartFile = wholeFile.substring(
+          wholeFile.indexOf(thisDate),
+          wholeFile.length
+        );
+        var currentDayIndex = lastPartFile.indexOf(thisDate);
+        var nextDay = parseInt(theDay, 10) + 1;
+        var nextDayFull = theYear + '/' + theMonth + '/' + nextDay.toString();
+        var nextDayIndex = lastPartFile.indexOf(nextDayFull);
+        var wholeDay = '';
+        var removeUser = '';
+        var combine = '';
+        //making sure I don't accidentally delete a user from a differnt date
+        if (nextDayIndex === -1) {
+          wholeDay = lastPartFile;
+          //if user is not found on this date.
+          if (wholeDay.indexOf(args[1]) === -1) {
+            return message.reply(
+              'The user you specified is not found on that date.'
+            );
+          }
+          //remove the user, remove the empty line, combine them together with the stuff before.
+          removeUserArr = wholeDay.split('\r\n');
+          removeUserArr.splice(removeUserArr.indexOf(args[1]), 1);
+          removeUser = removeUserArr.join('\r\n');
+          combine = firstPartFile + removeUser;
+        } else {
+          wholeDay = lastPartFile.substring(currentDayIndex, nextDayIndex - 1);
+          //if user is not found on this date.
+          if (wholeDay.indexOf(args[1]) === -1) {
+            return message.reply(
+              'The user you specified is not found on that date.'
+            );
+          }
+          //remove the user and combine them together with the stuff before and after.
+          removeUserArr = wholeDay.split('\r\n');
+          removeUserArr.splice(removeUserArr.indexOf(args[1]), 1);
+          removeUser = removeUserArr.join('\r\n');
+          combine =
+            firstPartFile +
+            removeUser +
+            lastPartFile.substring(nextDayIndex, lastPartFile.length);
         }
-        //remove the user and combine them together with the stuff before and after.
-        removeUserArr = wholeDay.split('\r\n');
-        removeUserArr.splice(removeUserArr.indexOf(args[1]), 1);
-        removeUser = removeUserArr.join('\r\n');
-        combine =
-          firstPartFile +
-          removeUser +
-          lastPartFile.substring(nextDayIndex, lastPartFile.length);
+        fs.writeFile('./logs/' + thisMonth + '.txt', combine, 'utf8', err => {
+          if (err) throw err;
+          console.log(
+            new Date() +
+              ` ${args[1]} has been removed from the recording on ${args[0]}`
+          );
+          return message.reply(
+            `${args[1]} has been removed from the recording on ${args[0]}`
+          );
+        });
       }
-      fs.writeFile('./logs/' + thisMonth + '.txt', combine, 'utf8', err => {
-        if (err) throw err;
-        console.log(
-          new Date() +
-            ` ${args[1]} has been removed from the recording on ${args[0]}`
-        );
-        return message.reply(
-          `${args[1]} has been removed from the recording on ${args[0]}`
-        );
-      });
     });
     //must close any opens or else an error can throw "too many files open"
     fs.close(fd, err => {
